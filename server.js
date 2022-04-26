4f7ed662c58c7cb35abca3d6f144608950fe4a82
asia <asia_jcrew96@yahoo.com>
2022-04-26 18:06:06 -0400

    updated server.js file to include notes

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const PORT = process.env.PORT || 3001;
-
+const app = express();
 const apiRoutes = require('./Develop/routes/apiRoutes');
 const htmlRoutes = require('./Develop/routes/htmlRoutes');
 
-const app = express();
-
-app.use(express.static('public'));
+// parse incoming string or array data
 app.use(express.urlencoded({extended: true}));
+// parse incoming JSON data
 app.use(express.json());
+// Make linked files/ assets (i.e. css, js scripts, etc.) available when loading the html in the browser (this illiminates the need for individual GET requests.)
+app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}. Welcome!`);
+    console.log(`API server now on port ${PORT}!`);
   });
+
+  // ^^ This is basically what the server.js file should always look like (lines 1- 19)
